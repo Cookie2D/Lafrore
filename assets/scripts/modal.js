1da24ce48ac777ee6deeaf2b293d972dceb0f090
Ihor Sharov <cookie50ds@gmail.com>
2023-04-03 14:01:03 +0300

    refactor modal script

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -2,10 +2,12 @@ const modal = document.getElementById("myModal");
 const close = document.getElementById("modal-close")
 
 const openModal = () => { modal.style.display = "block"; }
-close.onclick = () => {  modal.style.display = "none"; }
+const closeModal = () => { modal.style.display = "none"; }
 
-window.onclick = function(event) {
+close.addEventListener("click", closeModal);
+
+window.addEventListener("click", (event) => {
   if (event.target == modal) {
-    modal.style.display = "none";
+    closeModal();
   }
-}
+});
